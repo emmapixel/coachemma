04373575cc02962d0007bddf67a58975b4d00bf2
emmapixel <emmae-91@hotmail.com>
2024-02-21 17:49:38 +0100

    Started styling navbar

--- app/routes/_app.tsx
+++ app/routes/_app.tsx
@@ -13,11 +13,14 @@ export default function Layout() {
   const { user } = useLoaderData();
   return (
     <div>
-      <nav className="bg-gradient-to-br from-purple-400 via-purple-500 to-purple-500 w-full px-5">
+      <nav className="w-full">
+        <div className="bg-clam-shell w-full py-2 px-10">
+          <p className="text-white">coachemma.se@gmail.com</p>
+        </div>
         <div
-          className="w-full max-w-screen-lg mx-auto flex justify-between content-center py-3 ">
-          <Link className="text-white text-3xl font-bold" to={"/"}>Quote Wall</Link>
-          <div className="flex flex-row items-center justify-between gap-x-4 text-blue-50">
+          className="bg-white w-full py-4 px-20 flex justify-between">
+          <Link className= "text-3xl font-bold" to={"/"}>Coachemma</Link>
+          <div className="flex flex-row items-center justify-between gap-x-4">
             {
               user ? (
                 <>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'clam-shell': '#cdaca7',
+      },
+    },
   },
   plugins: [],
 } satisfies Config
