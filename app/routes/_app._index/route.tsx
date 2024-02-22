c7f2a5b99bf67a3c388981eb105fbe57ffddf9ed
emmapixel <emmae-91@hotmail.com>
2024-02-22 19:33:58 +0100

    Fixed with picture on landing page and fonts

--- app/routes/_app._index/route.tsx
+++ app/routes/_app._index/route.tsx
@@ -1,7 +1,14 @@
 export default function Index() {
     return (
-        <div>
-            <p>Detta är förstasidan</p>
+        <div className="bg-swiss-coffee opacity-70 flex">
+            <div className="w-1/2 h-80">
+                <img src="/images/flower.jpg" alt="A picture of a pink flower" className="h-full object-contain" />
+            </div>
+            <div className="w-1/2 h-80 flex flex-col items-center justify-center space-y-3">
+                <p className="text-clam-shell">Hejsan</p>
+                <p className="font-cutive text-2xl">Detta är förstasidan</p>
+                <p className="text-clam-shell">Hejsan</p>
+            </div>
         </div>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,10 @@ export default {
     extend: {
       colors: {
         'clam-shell': '#cdaca7',
+        'swiss-coffee': '#f9f8f7',
+      },
+      fontFamily: {
+        'cutive': ['Cutive', 'serif'],
       },
     },
   },
